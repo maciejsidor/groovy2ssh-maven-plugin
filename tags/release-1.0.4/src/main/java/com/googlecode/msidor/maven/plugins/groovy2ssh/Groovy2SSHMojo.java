a8364d96bea93484a57b1a85d8b2f6961890358c
maciejsidor <maciejsidor@04cb4f70-6c79-daea-290e-08245a7d903a>
2014-11-28 21:04:04 +0000

    [maven-release-plugin]  copy for tag release-1.0.4

---
+++ tags/release-1.0.4/src/main/java/com/googlecode/msidor/maven/plugins/groovy2ssh/Groovy2SSHMojo.java
@@ -0,0 +1,194 @@
+/*
+Copyright 2014 Maciej SIDOR [maciejsidor@gmail.com]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.	
+ */
+package com.googlecode.msidor.maven.plugins.groovy2ssh;
+
+import java.util.Map;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.slf4j.impl.StaticLoggerBinder;
+
+import com.googlecode.msidor.groovy2ssh.SSHManager;
+import com.jcraft.jsch.JSchException;
+
+/**
+ * @author Maciej SIDOR
+ * 
+ * Implementation of "execute" goal.
+ * 
+ * <p>
+ * Maven plugin that allows to execute shell commands via groovy script on remote host through ssh connection.
+ * This is basically only the maven plugin wrapper for com.googlecode.msidor.groovy2ssh library.
+ * See {@link com.googlecode.msidor.groovy2ssh} for more information.
+ * </p>
+ *
+ * @goal execute
+ */
+public class Groovy2SSHMojo extends AbstractMojo
+{
+
+    /**
+     * SSH user name. REQUIRED
+     * @parameter expression="${ssh.user.name}"
+     */     
+    String userName 			= null;
+
+    /**
+     * SSH user password. REQUIRED
+     * @parameter expression="${ssh.user.password}"
+     */     
+    String password 			= null;
+    
+    /**
+     * SSH server address. REQUIRED
+     * @parameter 
+     */         
+    String connectionIP 		= null;
+    
+    /**
+     * SSH known hosts files. REQUIRED
+     * @parameter 
+     */         
+    String knownHostsFileName 	= null;
+    
+    /**
+     * Groovy script to execute. REQUIRED
+     * @parameter 
+     */         
+    String scriptToExecute 		= null;
+    
+    /**
+     * Groovy script roots. REQUIRED
+     * @parameter 
+     */     
+    String scriptRoots 			= null;
+
+    /**
+     * SSH connection port.
+     * @parameter 
+     */         
+	int connectionPort = 22;
+
+    /**
+     * SSH connection timeout.
+     * @parameter 
+     */         	
+	int connectionTimeOut = 60000;
+	
+    /**
+     * SSH connection timeout.
+     * @parameter 
+     */             
+    int commandTimeOut = 3000;	
+    
+    /**
+     * Additional parameters that will be binded to groovy execution.
+     * 
+     * @parameter
+     */
+    private Map<String, Object> parameters = null;    
+        
+    /**
+     * Terminal type
+     * 
+     * @parameter
+     */
+    private String     ptyType = null;
+    
+    /**
+     * Terminal width, columns
+     * 
+     * @parameter
+     */
+    private int         col=0;
+    
+    /**
+     * Terminal height, rows
+     * 
+     * @parameter
+     */
+    private int         row=0;
+    
+    /**
+     * Terminal width, pixels
+     * 
+     * @parameter
+     */
+    private int         wp=0;
+    
+    /**
+     * Terminal height, pixel
+     * 
+     * @parameter
+     */
+    private int         hp=0;    
+    
+	
+    /**
+     * Implementation of "execute" goal.
+     * 
+     * @throws MojoExecutionException if groovy script was not successfully terminated
+     */
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException
+	{
+		
+        //Ignore the execution as the plugin is not configured at all for the project
+        if(connectionIP==null)
+            return;
+		
+	    StaticLoggerBinder.setMavenLog( getLog() );
+	    
+	    SSHManager instance    = null;
+	    boolean    result      = false; 
+	    	    	    	    
+		getLog().info("Preparing SSH connection...");
+		try
+		{
+		    instance = new SSHManager( userName, password, connectionIP, knownHostsFileName,connectionPort,connectionTimeOut,commandTimeOut,ptyType,col,row,wp,hp );
+		}
+		catch(JSchException ex)
+		{
+		    throw new MojoExecutionException(ex.getMessage());
+		}		        
+        
+        try
+        {		
+            getLog().info("Connecting to remote host...");
+            instance.connect();
+        }
+        catch(JSchException ex)
+        {
+            throw new MojoExecutionException(ex.getMessage());
+        }               
+
+       
+        getLog().info("Executing groovy script...");
+        result = instance.executeGroovyScript(parameters,scriptToExecute, scriptRoots);
+                                                
+        getLog().info("Disconnecting from remote host...");
+        instance.close();
+        
+        if(!result)
+            throw new MojoExecutionException("Groovy script execution failed. See log for more details");
+		
+	}
+	
+	
+	
+
+}
